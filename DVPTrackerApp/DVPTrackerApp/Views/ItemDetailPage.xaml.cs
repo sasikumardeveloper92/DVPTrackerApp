f01720b4df4f6b328ebcd1bcfd525d0b34382375
Stacksasikumar <developer1.sasikumar@gmail.com>
2024-06-06 12:34:23 +0530

    Add project files.

---
+++ DVPTrackerApp/DVPTrackerApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DVPTrackerApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DVPTrackerApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ DVPTrackerApp/DVPTrackerApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DVPTrackerApp.Models;
+using DVPTrackerApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DVPTrackerApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
